8728e77d1c1864c9bb72faaac17cc850e4da67ba
Tim Herzig <tim.herzig@hotmail.com>
2023-02-24 19:46:40 +0100

    updated splitting

--- src/create_k_splits.py
+++ src/create_k_splits.py
@@ -3,24 +3,24 @@ import pandas as pd
 
 pd.set_option("display.max_rows", None)
 
-splits = 19
-root = os.path.join('BabyCry', str(splits) + '_fold_split')
+splits = 10
+root = os.path.join('BabyCry2', str(splits) + '_fold_split')
 
-train_df = pd.read_csv('BabyCry/train.csv')
+train_df = pd.read_csv('BabyCry2/train.csv')
 train_df = train_df.sample(frac=1).reset_index(drop=True)
 
 ids = train_df['id'].value_counts().index.tolist()
 
-val_len = len(ids)//splits
+val_len = len(train_df.index)//splits
 print(val_len)
 
 for split in range(splits):
-    val_ids = ids[split*val_len:(split+1)*val_len]
+    # val_ids = ids[split*val_len:(split+1)*val_len]
     
-    val = train_df.loc[train_df['id'].isin(val_ids)]
-    train = train_df.loc[~train_df['id'].isin(val_ids)]
-    # val = train_df.loc[split*val_len:(split+1)*val_len]
-    # train = pd.concat([val, train_df]).drop_duplicates(keep=False)
+    # val = train_df.loc[train_df['id'].isin(val_ids)]
+    # train = train_df.loc[~train_df['id'].isin(val_ids)]
+    val = train_df.loc[split*val_len:(split+1)*val_len]
+    train = pd.concat([val, train_df]).drop_duplicates(keep=False)
 
     save_loc = os.path.join(root, str(split))
     os.makedirs(save_loc)
--- src/split_data.py
+++ src/split_data.py
@@ -8,7 +8,7 @@ from augment_data import augment_data
 
 pd.set_option("display.max_rows", None)
 
-df = pd.read_csv('BabyCry/babycry_extracted.csv')
+df = pd.read_csv('BabyCry2/babycry_extracted.csv')
 
 case_number = []
 for index, row in df.iterrows():
@@ -18,19 +18,26 @@ df['id'] = case_number
 
 # G13 37
 # J24 36
+# J31 31
+# G21 30
 # G29 22
 # J23 23
 # J13 17
 # G31 15
+# J28 11
+# G26 11
+# G11 9
+# J09 8
 
-test_ids = ['G13', 'J24', 'G29', 'J23', 'J13', 'G31']
+test_ids = ['G13', 'J24', 'J31', 'G21', 'G29', 'J23', 'J13', 'G31', 'J28', 'G26', 'G11', 'J09']
 test_df = df.loc[df['id'].isin(test_ids)]
 test_df['augmented'] = False
-test_df.to_csv('BabyCry/test.csv', index=False)
+test_df.to_csv('BabyCry2/test.csv', index=False)
 # print(test_df)
 
 train_df = df.loc[~df['id'].isin(test_ids)]
 train_df['augmented'] = False
+print(train_df['label'].value_counts())
 
 # train_df.to_csv('BabyCry/train.csv')
 # print(train_df['label'].value_counts())
@@ -40,11 +47,11 @@ for index, row in train_df.iterrows():
         augments = np.random.randint(3, size=2)
         cnt = 0
         for a in augments:
-            audio, sr = librosa.load('BabyCry/' + row['audio'])
+            audio, sr = librosa.load('BabyCry2/' + row['audio'])
             audio = augment_data(a, audio)
             audio_file = row['audio'][:-4] + '_aug' + str(cnt) +'.wav'
             cnt = cnt + 1
-            sf.write('BabyCry/' +  audio_file, np.ravel(audio), sr)
+            sf.write('BabyCry2/' +  audio_file, np.ravel(audio), sr)
             new_row = {'audio': audio_file, 'label': row['label'], 'id': row['id'], 'augmented': True}
             train_df = train_df.append(new_row, ignore_index=True)
     
@@ -52,12 +59,12 @@ for index, row in train_df.iterrows():
         augments = np.random.randint(3, size=1)
         for a in augments:
             if (bool(random.getrandbits(1))):
-                audio, sr = librosa.load('BabyCry/' + row['audio'])
+                audio, sr = librosa.load('BabyCry2/' + row['audio'])
                 audio = augment_data(a, audio)
                 audio_file = row['audio'][:-4] + '_aug.wav'
-                sf.write('BabyCry/' + audio_file, np.ravel(audio), sr)
+                sf.write('BabyCry2/' + audio_file, np.ravel(audio), sr)
                 new_row = {'audio': audio_file, 'label': row['label'], 'id': row['id'], 'augmented': True}
                 train_df = train_df.append(new_row, ignore_index=True)
             
 
-train_df.to_csv('BabyCry/train.csv', index=False)
+train_df.to_csv('BabyCry2/train.csv', index=False)
